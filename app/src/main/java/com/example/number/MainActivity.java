16660d56e6c7ae02826417f10dd631c4f534be10
Kseniya Nesterova <ksenya-nesterova@yandex.ru>
2019-04-04 03:24:32 +0300

    Нестерова ПИ3-1

--- app/src/main/java/com/example/number/MainActivity.java
+++ app/src/main/java/com/example/number/MainActivity.java
@@ -6,12 +6,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tvInfo;
+    TextView tvInfo, mode;
     EditText etInput;
-    Button bControl;
+    Button bControl, exit, hardMode, easyMode;
+    Random rand = new Random();
+    int number = rand.nextInt(100) + 1;
+    int maxValue = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +25,69 @@ public class MainActivity extends AppCompatActivity {
         tvInfo = findViewById(R.id.textView);
         etInput = findViewById(R.id.editText);
         bControl = findViewById(R.id.button);
+        exit = findViewById(R.id.exit);
+        hardMode = findViewById(R.id.hardMode);
+        easyMode = findViewById(R.id.easyMode);
+        mode = findViewById(R.id.mode);
+        mode.setText(getString(R.string.easyMode));
 
     }
 
     public void onClick(View view) {
-        int value = Integer.parseInt(etInput.getText().toString());
-        tvInfo.setText(getResources().getString(R.string.ahead));
+
+
+        View.OnClickListener clickListenerExit = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        };
+
+        exit.setOnClickListener(clickListenerExit);
+
+
+        View.OnClickListener clickListenerHardMode = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                number = rand.nextInt(10000) + 1;
+                maxValue = 10000;
+                mode.setText(getString(R.string.mode));
+            }
+        };
+        hardMode.setOnClickListener(clickListenerHardMode);
+
+        View.OnClickListener clickListenerEasyMode = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                number = rand.nextInt(100) + 1;
+                maxValue = 100;
+                mode.setText(getString(R.string.easyMode));
+            }
+        };
+        easyMode.setOnClickListener(clickListenerEasyMode);
+
+        if (etInput.getText().toString().length()==0){
+            tvInfo.setText(getString(R.string.suggestion));
+        }
+        else {
+            int value = Integer.parseInt(etInput.getText().toString());
+            if ((value < 1) || (value > maxValue)) {
+                tvInfo.setText(getString(R.string.errorValue));
+            }
+            else if (value < number) {
+                tvInfo.setText("Больше");
+            }
+            else if (value > number) {
+                tvInfo.setText("Меньше");
+            }
+            else {
+                tvInfo.setText("Ура! Угадал!");
+            }
+
+        }
+
     }
+
 }
 
